1b378e3fe4b8e7a571c38c2efd9a521489f86509
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-02-24 18:32:23 +0530

    no remember

--- Assets/NumberScript.cs
+++ Assets/NumberScript.cs
@@ -7,6 +7,8 @@ public class NumberScript : MonoBehaviour
 {
     private TMP_Text myText;
     [SerializeField] private GameObject number_4;
+    [SerializeField] private GameObject number_6;
+    [SerializeField] private float radius = 1f;
     
     // Start is called before the first frame update
     void Start()
@@ -17,16 +19,24 @@ public class NumberScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1f);
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, radius);
 
         foreach (Collider2D col in collider)
         {
             if(col.transform.root != transform)
             {
                 TMP_Text text = col.GetComponentInChildren<TMP_Text>();
-                if (text != null && text.text == myText.text)
+                if (text != null && text.text == "3" && myText.text == "3")
                 {
+                    Destroy(col.gameObject);
+                    Instantiate(number_6, transform.position, Quaternion.identity);
+                    Destroy(gameObject);
+                }
+                else if (text != null && text.text == "2" && myText.text == "2")
+                {
+                    Destroy(col.gameObject);
                     Instantiate(number_4, transform.position, Quaternion.identity);
+                    Destroy(gameObject);
                 }
             }
         }
--- Assets/WordScript.cs
+++ Assets/WordScript.cs
@@ -8,16 +8,19 @@ public class WordScript : MonoBehaviour
 {
     [SerializeField] private string letter;
     [SerializeField] private GameObject number_2;
+    [SerializeField] private float radius = 1f;
 
     private void OnDrawGizmos()
     {
         Handles.color = Color.red;
-        Handles.DrawWireDisc(transform.position, transform.forward, 1f);
+        Handles.DrawWireDisc(transform.position, transform.forward, 2f);
     }
 
     private void Update()
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1f);
+        bool destroyMe = false;
+
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, radius);
 
         foreach (Collider2D col in collider)
         {
@@ -26,9 +29,20 @@ public class WordScript : MonoBehaviour
                 TMP_Text text = col.GetComponentInChildren<TMP_Text>();
                 if (text.text == letter)
                 {
+                    Destroy(col.gameObject);
                     Instantiate(number_2, transform.position, Quaternion.identity);
+                    destroyMe = true;
                 }
             }
+        }
+
+        if(destroyMe)
+        {
+            foreach (Collider2D col in collider)
+            {
+                Destroy(col.gameObject);
+            }
+            Destroy(gameObject);
         }
     }
 }
